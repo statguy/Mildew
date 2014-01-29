d91fe54a07fd643b350698119368fc4e2c63d9d3
statguy <jvj@iki.fi>
2014-01-30 00:47:36 +0700

    ...

--- inst/process/estimate.R
+++ inst/process/estimate.R
@@ -60,7 +60,7 @@ task62 <- function() estimateRandomEffectModel(col, connectivity.scale=col.conne
 task63 <- function() estimateRandomEffectModel(ext, connectivity.scale=ext.connectivity.scale, fixed.effects=ext.fixed.effects, mesh.params=ext.mesh.params, type="spatiotemporal")
 
 cnpClusterStartRemote(runParallel=runParallel, hosts=cnpClusterGetHostsUkko(maxNodes=6*3), outFile=file.path(basePath, "estimate.log"))
-cnpClusterEval({library(Mildew); library(INLA) })
+cnpClusterEval({ library(Mildew); library(INLA) })
 cnpClusterExport(c("occ", "col", "ext",
                    "occ.mesh.params", "col.mesh.params", "ext.mesh.params",
                    "occ.connectivity.scale", "col.connectivity.scale", "ext.connectivity.scale",
