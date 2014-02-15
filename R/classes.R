ab6759f026699cd3a288395ada87e1c98a687f78
Jussi Jousimo <jvj@iki.fi>
2014-02-15 18:56:14 +0200

    fix

--- R/classes.R
+++ R/classes.R
@@ -445,8 +445,14 @@ OccupancyMildew <- setRefClass(
       library(plyr)
       library(INLA)
       message("Estimating mu...")
-      index.pred <- inla.stack.index(data.stack, "pred")$data
-      data$mu <<- laply(result$marginals.linear.predictor[index.pred], function(x) inla.emarginal(invlogit, x), .progress="text")      
+      
+      if (is.null(data.stack) | inherits(data.stack, "uninitializedField")) {
+        data$mu <<- laply(result$marginals.linear.predictor, function(x) inla.emarginal(invlogit, x), .progress="text")
+      }
+      else {
+        index.pred <- inla.stack.index(data.stack, "pred")$data
+        data$mu <<- laply(result$marginals.linear.predictor[index.pred], function(x) inla.emarginal(invlogit, x), .progress="text")
+      }
       invisible(.self)
     },
     
